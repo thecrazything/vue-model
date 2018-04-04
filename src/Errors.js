c453d1a34fe3c999416e6b169e632cacc601afd5
Joakim Boström <joakim.bostrom43@gmail.com>
2018-04-04 19:29:01 +0200

    Update Errors.js

--- src/Errors.js
+++ src/Errors.js
@@ -1,7 +1,7 @@
 var _ = require('lodash');
-var Vue = require('vue');
+import Vue from 'vue';
 
-module.exports = class Errors {
+export defaults {
 
     constructor(errors) {
         this.set(errors);
